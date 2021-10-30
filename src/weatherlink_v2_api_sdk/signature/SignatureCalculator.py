8490bf77471e8272178b41b9d8b5c1eb583736e0
Jeremy Wall <jeremyw@davisinstruments.com>
2021-10-29 23:20:31 -0700

    temp wip

--- src/weatherlink_v2_api_sdk/signature/SignatureCalculator.py
+++ src/weatherlink_v2_api_sdk/signature/SignatureCalculator.py
@@ -1,8 +1,16 @@
 import collections
 import hashlib
 import hmac
-import time
 
 class SignatureCalculator:
 
-    def calculate_stations_signature(self):
+    def calculate_stations_signature(self, api_key, api_secret, api_request_timestamp, station_ids = []):
+        parameters_to_hash = {
+            "api-key": api_key,
+            "t": api_request_timestamp
+        }
+        return calculate_signature(api_secret, parameters_to_hash)
+
+    def calculate_signature(api_secret, parameters_to_hash):
+
+        return ""
