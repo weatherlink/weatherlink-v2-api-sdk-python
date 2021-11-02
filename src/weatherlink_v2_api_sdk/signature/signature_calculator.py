7ee467488c3c5611168b7dcd2ddf774882420089
Jeremy Wall <jeremyw@davisinstruments.com>
2021-11-01 21:19:17 -0700

    add doc comments

--- src/weatherlink_v2_api_sdk/signature/signature_calculator.py
+++ src/weatherlink_v2_api_sdk/signature/signature_calculator.py
@@ -2,8 +2,28 @@ import hashlib
 import hmac
 
 class SignatureCalculator:
+    """A collection of API Signature calculation functions.
 
-    def calculate_stations_signature(self, api_key, api_secret, api_request_timestamp, station_ids = []):
+        Typical usage example:
+
+        ```
+        sc = SignatureCalculator()
+        api_signature = sc.calculate_stations_signature(api_key, api_secret, timestamp)
+        ```
+    """
+
+    def calculate_stations_signature(self, api_key: str, api_secret: str, api_request_timestamp: int, station_ids: list[int] = []) -> str:
+        """Computes the API Signature for an API call to /stations given the specific parameters.
+
+        Args:
+            api_key: the API Key
+            api_secret: the API Secret
+            api_request_timestamp: the Unix timestamp for when the API request is being made
+            station_ids: integer list of up to 100 station ID numbers
+
+        Returns:
+            The API Signature string
+        """
         parameters_to_hash = {
             "api-key": api_key,
             "t": api_request_timestamp
@@ -12,7 +32,18 @@ class SignatureCalculator:
             parameters_to_hash["station-ids"] = ",".join(map(str, station_ids))
         return self.__calculate_signature(api_secret, parameters_to_hash)
 
-    def calculate_nodes_signature(self, api_key, api_secret, api_request_timestamp, node_ids = []):
+    def calculate_nodes_signature(self, api_key: str, api_secret: str, api_request_timestamp: int, node_ids: list[int] = []) -> str:
+        """Computes the API Signature for an API call to /nodes given the specific parameters.
+
+        Args:
+            api_key: the API Key
+            api_secret: the API Secret
+            api_request_timestamp: the Unix timestamp for when the API request is being made
+            node_ids: integer list of up to 100 node ID numbers
+
+        Returns:
+            The API Signature string
+        """
         parameters_to_hash = {
             "api-key": api_key,
             "t": api_request_timestamp
@@ -21,7 +52,18 @@ class SignatureCalculator:
             parameters_to_hash["node-ids"] = ",".join(map(str, node_ids))
         return self.__calculate_signature(api_secret, parameters_to_hash)
 
-    def calculate_sensors_signature(self, api_key, api_secret, api_request_timestamp, sensor_ids = []):
+    def calculate_sensors_signature(self, api_key: str, api_secret: str, api_request_timestamp: int, sensor_ids: list[int] = []) -> str:
+        """Computes the API Signature for an API call to /sensors given the specific parameters.
+
+        Args:
+            api_key: the API Key
+            api_secret: the API Secret
+            api_request_timestamp: the Unix timestamp for when the API request is being made
+            sensor_ids: integer list of up to 100 sensor ID numbers
+
+        Returns:
+            The API Signature string
+        """
         parameters_to_hash = {
             "api-key": api_key,
             "t": api_request_timestamp
@@ -30,7 +72,18 @@ class SignatureCalculator:
             parameters_to_hash["sensor-ids"] = ",".join(map(str, sensor_ids))
         return self.__calculate_signature(api_secret, parameters_to_hash)
 
-    def calculate_sensor_activity_signature(self, api_key, api_secret, api_request_timestamp, sensor_ids = []):
+    def calculate_sensor_activity_signature(self, api_key: str, api_secret: str, api_request_timestamp: int, sensor_ids: list[int] = []) -> str:
+        """Computes the API Signature for an API call to /sensor-activity given the specific parameters.
+
+        Args:
+            api_key: the API Key
+            api_secret: the API Secret
+            api_request_timestamp: the Unix timestamp for when the API request is being made
+            sensor_ids: integer list of up to 100 sensor ID numbers
+
+        Returns:
+            The API Signature string
+        """
         parameters_to_hash = {
             "api-key": api_key,
             "t": api_request_timestamp
@@ -39,14 +92,35 @@ class SignatureCalculator:
             parameters_to_hash["sensor-ids"] = ",".join(map(str, sensor_ids))
         return self.__calculate_signature(api_secret, parameters_to_hash)
 
-    def calculate_sensor_catalog_signature(self, api_key, api_secret, api_request_timestamp):
+    def calculate_sensor_catalog_signature(self, api_key: str, api_secret: str, api_request_timestamp: int) -> str:
+        """Computes the API Signature for an API call to /sensor-catalog given the specific parameters.
+
+        Args:
+            api_key: the API Key
+            api_secret: the API Secret
+            api_request_timestamp: the Unix timestamp for when the API request is being made
+
+        Returns:
+            The API Signature string
+        """
         parameters_to_hash = {
             "api-key": api_key,
             "t": api_request_timestamp
         }
         return self.__calculate_signature(api_secret, parameters_to_hash)
 
-    def calculate_current_signature(self, api_key, api_secret, api_request_timestamp, station_id):
+    def calculate_current_signature(self, api_key: str, api_secret: str, api_request_timestamp: int, station_id: int) -> str:
+        """Computes the API Signature for an API call to /current given the specific parameters.
+
+        Args:
+            api_key: the API Key
+            api_secret: the API Secret
+            api_request_timestamp: the Unix timestamp for when the API request is being made
+            station_id: the station ID
+
+        Returns:
+            The API Signature string
+        """
         parameters_to_hash = {
             "api-key": api_key,
             "t": api_request_timestamp,
@@ -54,7 +128,20 @@ class SignatureCalculator:
         }
         return self.__calculate_signature(api_secret, parameters_to_hash)
 
-    def calculate_historic_signature(self, api_key, api_secret, api_request_timestamp, station_id, start_timestamp, end_timestamp):
+    def calculate_historic_signature(self, api_key: str, api_secret: str, api_request_timestamp: int, station_id: int, start_timestamp: int, end_timestamp: int) -> str:
+        """Computes the API Signature for an API call to /historic given the specific parameters.
+
+        Args:
+            api_key: the API Key
+            api_secret: the API Secret
+            api_request_timestamp: the Unix timestamp for when the API request is being made
+            station_id: the station ID
+            start_timestamp: the Unix timestamp marking the beginning of the time window for the data requested
+            end_timestamp: the Unix timestamp marking the end of the time window for the data requested
+
+        Returns:
+            The API Signature string
+        """
         parameters_to_hash = {
             "api-key": api_key,
             "t": api_request_timestamp,
